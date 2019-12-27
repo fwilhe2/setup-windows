91ec95c0baeb62de3468784b114a745ddd790c5c
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2019-12-27 17:16:52 +0100

    Git aliases

--- config.ps1
+++ config.ps1
@@ -1,2 +1,5 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/fwilhe2/dotfiles/master/vs-code-settings.json -OutFile $env:APPDATA\Code\User\settings.json
 Copy-Item .\gitconfig $env:USERPROFILE\.gitconfig
+
+New-Item -ItemType Directory $env:USERPROFILE\Documents\WindowsPowerShell -Force
+Copy-Item -Path .\pwsh-cfg.ps1 -Destination $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
