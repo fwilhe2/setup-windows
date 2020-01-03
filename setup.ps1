0b32e501b7fb02d3b513dd76e22575df4de6cbe0
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-01-03 17:56:15 +0100

    install powershell core and download sapmachine

--- setup.ps1
+++ setup.ps1
@@ -3,3 +3,9 @@ choco feature enable -n allowGlobalConfirmation
 choco install git
 choco install dotnetcore-sdk
 choco install vscode
+
+dotnet tool install --global PowerShell
+
+Invoke-WebRequest -Uri https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.5/sapmachine-jdk-11.0.5_windows-x64_bin.zip
+Expand-Archive -Path sapmachine-jdk-11.0.5_windows-x64_bin.zip
+find .
