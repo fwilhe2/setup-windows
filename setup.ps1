486252398ecac3b7dc43de85a8170ea0dc2b9a03
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2020-01-03 18:02:49 +0100

    update

--- setup.ps1
+++ setup.ps1
@@ -6,6 +6,6 @@ choco install vscode
 
 dotnet tool install --global PowerShell
 
-Invoke-WebRequest -Uri https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.5/sapmachine-jdk-11.0.5_windows-x64_bin.zip
-Expand-Archive -Path sapmachine-jdk-11.0.5_windows-x64_bin.zip
+Invoke-WebRequest -Uri https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.5/sapmachine-jdk-11.0.5_windows-x64_bin.zip -OutFile jdk.zip
+Expand-Archive -Path jdk.zip
 find .
