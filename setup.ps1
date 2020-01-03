d7c367fe0d2520f6b73d35b7539593ea169c8726
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2020-01-03 17:40:44 +0100

    remove jabba

--- setup.ps1
+++ setup.ps1
@@ -3,10 +3,3 @@ choco feature enable -n allowGlobalConfirmation
 choco install git
 choco install dotnetcore-sdk
 choco install vscode
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-Expression (
-  Invoke-WebRequest https://github.com/shyiko/jabba/raw/master/install.ps1 -UseBasicParsing
-).Content
-
-jabba install adopt@1.8-0
