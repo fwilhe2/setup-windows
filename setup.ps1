2d56af25f7115100b4d0a747c5911727e13fcd9a
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2020-01-19 19:42:33 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,3 @@
-choco feature enable -n allowGlobalConfirmation
-
-choco install git
-choco install dotnetcore-sdk
-choco install vscode
-
-dotnet tool install --global PowerShell
 
 Invoke-WebRequest -Uri https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.5/sapmachine-jdk-11.0.5_windows-x64_bin.zip -OutFile jdk.zip
 Expand-Archive -Path jdk.zip
