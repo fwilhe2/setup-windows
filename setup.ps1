69a6f6d24620001a05ef70af5e4dd91228f29457
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2020-01-19 19:42:14 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -9,7 +9,7 @@ dotnet tool install --global PowerShell
 Invoke-WebRequest -Uri https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.5/sapmachine-jdk-11.0.5_windows-x64_bin.zip -OutFile jdk.zip
 Expand-Archive -Path jdk.zip
 
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $pwd + "jdk\sapmachine-jdk-11.0.5\bin\", "Machine")
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";" + $pwd + "\jdk\sapmachine-jdk-11.0.5\bin\", "Machine")
 
 pwsh --version
 java.exe -version
